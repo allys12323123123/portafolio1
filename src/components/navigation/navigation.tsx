d33ab03233e12f25e9cc3dc4a4298a5cbb35ebb2
Mike-cheek <michele00.pulvirenti@gmail.com>
2022-01-10 17:18:44 +0100

    Fixed key list

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -32,12 +32,12 @@ const Navigation = ({onClick}: NavigationProps): JSX.Element => {
         <>
             <div className={styles.navDesktop}>
                 {items.map((item) => {
-                    return <NavItem text={item.text} path={item.path} isHref={item.isHref} />
+                    return <NavItem text={item.text} path={item.path} isHref={item.isHref} key={item.text} />
                 })}
             </div>
             <div className={styles.navMobile}>
                 {items.map((item) => {
-                    return <NavItem text={item.text} path={item.path} onClick={onClick} isHref={item.isHref} />
+                    return <NavItem text={item.text} path={item.path} onClick={onClick} isHref={item.isHref} key={item.text} />
                 })}
             </div>
         </>
