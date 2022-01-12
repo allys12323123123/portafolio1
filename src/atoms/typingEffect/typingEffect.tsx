2697f106028a9149bc9bc2782bad20183abbd2cb
Mike-cheek <michele00.pulvirenti@gmail.com>
2022-01-12 15:59:54 +0100

    Edited typing animation

--- src/atoms/typingEffect/typingEffect.tsx
+++ src/atoms/typingEffect/typingEffect.tsx
@@ -4,6 +4,8 @@ import sleep from '../../utilities/sleep';
 import * as styles from './typingEffect.module.scss'
 import { TypingEffectProps } from './typingEffect.types';
 
+const alphabet = Array.from("abcdefghijklmnopqrstuvz");
+
 
 const TypingEffect = ({initialText, heading = false, fast = true}: TypingEffectProps): JSX.Element => {
     const array: string[] = Array.from(initialText);
@@ -11,19 +13,33 @@ const TypingEffect = ({initialText, heading = false, fast = true}: TypingEffectP
 
     let tmpString: string = '';
     const min = 0;
-    const max = fast? 10 : 200;
+    const max = fast? 10 : 100;
 
     const write = async (): Promise<void> => {
         await sleep(500);
         for(let i = 0; i < array.length; i++){
+            tmpString += alphabet[randomLetter()]
+            setText(tmpString);
+            await sleep(randomTime())
+            tmpString = tmpString.slice(0, -1);
+            tmpString += alphabet[randomLetter()]
+            setText(tmpString);
+            await sleep(randomTime())
+            tmpString = tmpString.slice(0, -1)
             tmpString += array[i];
             setText(tmpString);
-            await sleep(random());
+            await sleep(randomTime());
         }
     }
 
-    const random = (): number => {
+    const randomTime = (): number => {
         let rand = min + Math.random() * (max - min);
+        rand = Math.floor(rand)
+        return rand;
+    }
+
+    const randomLetter = (): number => {
+        let rand = 0 + Math.random() * (alphabet.length - 1);
         rand = Math.floor(rand)
         return rand;
     }
@@ -31,6 +47,10 @@ const TypingEffect = ({initialText, heading = false, fast = true}: TypingEffectP
     useEffect(() => {
         write();
     }, [])
+
+    useEffect(() => {
+        console.log(text)
+    }, [text])
 
     return (
         <>
