b2eddeaa05e928ccb2af6887eeaf0c5aebfd1b4a
Mike-cheek <michele00.pulvirenti@gmail.com>
2022-01-16 13:23:23 +0100

    Made bar tsx

--- src/atoms/bar/bar.tsx
+++ src/atoms/bar/bar.tsx
@@ -2,27 +2,15 @@ import React, { useState, useEffect, useRef } from 'react'
 
 import * as styles from './bar.module.scss'
 
-//import { BarProps } from './bar.types';
+import { BarProps } from './bar.types';
 
-import styled, { keyframes } from 'styled-components';
-
+//@ts-ignore
 import isOnScreen from '../../utilities/isOnScreen';
 import { useThemeContext } from '../../utilities/themeContext'
 
 //const fill = keyframes`100%{width: ${(props) => props.percentage || "0%"};}`
 
-const BarDiv = styled.div`
-        position: relative;
-        height: 9px;
-        border-radius: 10px;
-        background-color: var(--orange);
-        will-change: width;
-        width: ${(props) => props.percentage || "0%"};
-        -webkit-transition: width ${(props) => props.rand || 2}s ease-in-out;
-        transition: width ${(props) => props.rand || 2}s ease-in-out;
-    `
-
-const Bar = ({name, percentage}) => {
+const Bar = ({name, percentage}: BarProps) => {
     const barRef = useRef(null)
     const check = isOnScreen(barRef, false)
 
@@ -32,6 +20,11 @@ const Bar = ({name, percentage}) => {
 
     const [perc, setPerc] = useState("0%");
     const [rand, setRand] = useState(2);
+
+    const BarStyle = {
+        width: isVisible? perc : "0%",
+        transition: `${rand}s ease-in-out`
+    }
 
     const changePerc = () => {
         setPerc(percentage);
@@ -58,7 +51,7 @@ const Bar = ({name, percentage}) => {
                 <span>{percentage}</span>
             </div>
             <div className={`${styles.barWrap} ${theme === "dark" ? null : styles.light}`} ref={barRef}>
-                <BarDiv percentage={isVisible? perc : "0%"} rand={rand} />
+                <div className={styles.bar} style={BarStyle} />
             </div>
         </div>
     )
